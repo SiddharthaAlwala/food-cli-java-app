b40a99b66f2fe769aa95e4f89029d10672e7e27b
siddhartha <siddhartha1197@gmail.com>
2024-07-11 18:13:18 -0500

    feat: Implimenting main function.

--- src/com/javaapplication/foodcliapp/Main.java
+++ src/com/javaapplication/foodcliapp/Main.java
@@ -1,24 +1,9 @@
 package com.javaapplication.foodcliapp;
 
-import com.javaapplication.foodcliapp.repository.CustomerRepository;
-import com.javaapplication.foodcliapp.repository.DishRepository;
-import com.javaapplication.foodcliapp.repository.RestaurantRepository;
 import com.javaapplication.foodcliapp.ui.Menu;
-import com.javaapplication.foodcliapp.util.CsvReader;
 
 public class Main {
     public static void main(String[] args) {
-        //CsvReader csvReader = new CsvReader();
-        //System.out.println(csvReader.reaCustomersFromCsv());
-        //System.out.println(csvReader.readDishesFromCsv());
-        //System.out.println(csvReader.readRestaurantsFromCsv());
-        //CustomerRepository customerRepository = new CustomerRepository();
-        //DishRepository dishRepository = new DishRepository();
-        //RestaurantRepository restaurantRepository = new RestaurantRepository();
-        //System.out.println(customerRepository.getAllCustomers());
-        //System.out.println(dishRepository.getAllDishes());
-        //System.out.println(restaurantRepository.getAllRestaurents());
-
         Menu menu = new Menu();
         menu.displayMainMenu();
 
